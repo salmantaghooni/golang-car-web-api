7c5067b6c95774b908ad4608db4ecd258c9a65fa
Salman Taghouni <salman.taghooni@gmail.com>
2024-01-17 13:54:42 +0330

    logger for request and response

--- src/api/api.go
+++ src/api/api.go
@@ -26,8 +26,8 @@ func InitServer(cfg *config.Config) {
 }
 
 func RegisterMiddlewares(r *gin.Engine, cfg *config.Config) {
-	// r.Use(gin.Logger(), gin.Recovery(), middlewares.TestMiddleware(), middlewares.LimitByRequest())
 	r.Use(middlewares.DefaultStracturedLogger(cfg))
+	r.Use(middlewares.LimitByRequest())
 	r.Use(middlewares.Cors(cfg))
 	r.Use(gin.Logger(), gin.Recovery())
 }
